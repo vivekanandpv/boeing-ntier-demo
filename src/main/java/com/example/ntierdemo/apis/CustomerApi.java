32f9b625303093d16307dd46840860c378507d8a
Vivekanand P V <vivekanandpv@outlook.com>
2024-05-14 10:46:10 +0530

    Api now uses the service

--- src/main/java/com/example/ntierdemo/apis/CustomerApi.java
+++ src/main/java/com/example/ntierdemo/apis/CustomerApi.java
@@ -1,5 +1,6 @@
 package com.example.ntierdemo.apis;
 
+import com.example.ntierdemo.services.CustomerService;
 import com.example.ntierdemo.viewmodels.CustomerCreateViewModel;
 import com.example.ntierdemo.viewmodels.CustomerUpdateViewModel;
 import com.example.ntierdemo.viewmodels.CustomerViewModel;
@@ -11,29 +12,35 @@ import java.util.List;
 @RestController
 @RequestMapping("api/v1/customers")
 public class CustomerApi {
+    private final CustomerService customerService;
+
+    public CustomerApi(CustomerService customerService) {
+        this.customerService = customerService;
+    }
 
     @GetMapping
     public ResponseEntity<List<CustomerViewModel>> getAll() {
-
+        return ResponseEntity.ok(customerService.getAll());
     }
 
     @GetMapping("{id}")
     public ResponseEntity<CustomerViewModel> getById(@PathVariable int id) {
-
+        return ResponseEntity.ok(customerService.getById(id));
     }
 
     @PostMapping
     public ResponseEntity<CustomerViewModel> create(@RequestBody CustomerCreateViewModel viewModel) {
-
+        return ResponseEntity.ok(customerService.create(viewModel));
     }
 
     @PutMapping("{id}")
     public ResponseEntity<CustomerViewModel> update(@PathVariable int id, @RequestBody CustomerUpdateViewModel viewModel) {
-
+        return ResponseEntity.ok(customerService.update(id, viewModel));
     }
 
     @DeleteMapping("{id}")
     public ResponseEntity<?> deleteById(@PathVariable int id) {
-
+        customerService.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }

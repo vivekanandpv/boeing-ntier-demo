063688c72d759e21a599ea722a27542d828bbf73
Vivekanand P V <vivekanandpv@outlook.com>
2024-05-14 15:53:40 +0530

    service implementation

--- src/main/java/com/example/ntierdemo/apis/ErrorHandlerAdvice.java
+++ src/main/java/com/example/ntierdemo/apis/ErrorHandlerAdvice.java
@@ -1,5 +1,6 @@
 package com.example.ntierdemo.apis;
 
+import com.example.ntierdemo.exceptions.RecordNotFoundException;
 import com.example.ntierdemo.utils.AppUtils;
 import com.example.ntierdemo.viewmodels.ValidationErrorPageViewModel;
 import org.springframework.http.ResponseEntity;
@@ -7,10 +8,18 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.Map;
+
 @ControllerAdvice
 public class ErrorHandlerAdvice {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ValidationErrorPageViewModel> handleValidationException(MethodArgumentNotValidException exception) {
         return ResponseEntity.badRequest().body(AppUtils.getValidationErrorPage(exception));
+    }
+
+    @ExceptionHandler(RecordNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleRecordNotFoundException(RecordNotFoundException exception) {
+        Map<String, String> map = Map.of("message", exception.getMessage());
+        return ResponseEntity.badRequest().body(map);
     }
 }
--- src/main/java/com/example/ntierdemo/services/CustomerServiceImplementation.java
+++ src/main/java/com/example/ntierdemo/services/CustomerServiceImplementation.java
@@ -1,12 +1,16 @@
 package com.example.ntierdemo.services;
 
+import com.example.ntierdemo.exceptions.RecordNotFoundException;
+import com.example.ntierdemo.models.Customer;
 import com.example.ntierdemo.repositories.CustomerRepository;
 import com.example.ntierdemo.viewmodels.CustomerCreateViewModel;
 import com.example.ntierdemo.viewmodels.CustomerUpdateViewModel;
 import com.example.ntierdemo.viewmodels.CustomerViewModel;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CustomerServiceImplementation implements CustomerService {
@@ -18,26 +22,54 @@ public class CustomerServiceImplementation implements CustomerService {
 
     @Override
     public List<CustomerViewModel> getAll() {
-        return List.of();
+        return customerRepository.findAll()
+                .stream()
+                .map(this::toViewModel)
+                .collect(Collectors.toList());
     }
 
     @Override
     public CustomerViewModel getById(int id) {
-        return null;
+        return toViewModel(getEntityById(id));
     }
 
     @Override
     public CustomerViewModel create(CustomerCreateViewModel viewModel) {
-        return null;
+        return toViewModel(customerRepository.saveAndFlush(toEntity(viewModel)));
     }
 
     @Override
     public CustomerViewModel update(int id, CustomerUpdateViewModel viewModel) {
-        return null;
+        Customer entity = getEntityById(id);
+
+        BeanUtils.copyProperties(viewModel, entity);
+
+        return toViewModel(customerRepository.saveAndFlush(entity));
     }
 
     @Override
     public void deleteById(int id) {
+        Customer entity = getEntityById(id);
+        customerRepository.delete(entity);
+    }
 
+    private Customer getEntityById(int id) {
+        return customerRepository.findById(id)
+                .orElseThrow(() -> new RecordNotFoundException(
+                                String.format("Could not find the customer with id: %d", id)
+                        )
+                );
+    }
+
+    private CustomerViewModel toViewModel(Customer entity) {
+        CustomerViewModel viewModel = new CustomerViewModel();
+        BeanUtils.copyProperties(entity, viewModel);
+        return viewModel;
+    }
+
+    private Customer toEntity(CustomerCreateViewModel viewModel) {
+        Customer entity = new Customer();
+        BeanUtils.copyProperties(viewModel, entity);
+        return entity;
     }
 }
